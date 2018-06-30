bff5014691ebd7625922636fd6509df7b3193d2b
IslamTareq0012 <31237314+IslamTareq0012@users.noreply.github.com>
2018-06-30 23:47:12 +0200

    split app to compontents

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Joke } from '../../Models/joke';
 
 @Component({
   selector: 'app-joke',
@@ -6,11 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./joke.component.css']
 })
 export class JokeComponent implements OnInit {
-  setup: string;
-  punchline: string;
+  @Input() joke: Joke;
   constructor() {
-    this.setup = "What did the cheese say when it looked in the mirror?";
-    this.punchline = "Halloumi (Hello Me)";
   }
 
   ngOnInit() {
