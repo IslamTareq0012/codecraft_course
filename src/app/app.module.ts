c822879180addd4203b5a1fb55c2951bde3fe50f
IslamTareq0012 <31237314+IslamTareq0012@users.noreply.github.com>
2018-07-07 14:38:45 +0200

    rxjs finished with angular 6 updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { AppComponent } from './app.component';
 import { HeaderokeFormComponent } from './headeroke-form/headeroke-form.component';
 import { JokeComponent } from './joke/joke.component';
 import { JokeListComponent } from './joke-list/joke-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CardHoverDirective } from '../Directives/cardHover';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderokeFormComponent,
     JokeComponent,
     JokeListComponent,
-    CardHoverDirective
+    CardHoverDirective,
+    ReactiveFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,48 @@
+import { NgModule, Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+import 'rxjs/Rx';
+import { filter, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent implements OnInit {
+
+  form: FormGroup;
+  comment = new FormControl("", Validators.required);
+  name = new FormControl("", Validators.required);
+  email = new FormControl("", [
+    Validators.required,
+    Validators.pattern("[^ @]*@[^ @]*")
+  ]);
+
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      "comment": this.comment,
+      "name": this.name,
+      "email": this.email
+    });
+    this.form.valueChanges.pipe(
+      filter(data => this.form.valid),
+      map(data => {
+        data.comment = data.comment.replace(/<(?:.|\n)*?>/gm, ' ');
+        return data
+      }),
+      map(data => {
+        data.lastUpdateTS = new Date();
+        return data
+      })
+    ).subscribe(data => console.log(JSON.stringify(data)));
+
+  }
+
+  ngOnInit() {
+  }
+  onSubmit() {
+    console.log("Form submitted!");
+  }
+
+}
